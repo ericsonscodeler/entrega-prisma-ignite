563d863859a5b8ae910a8a369f0cd2d9a1d8deb7
Ericson Scodeler <ericson.br@hotmail.com>
2022-05-21 22:00:18 -0300

    criar sessão para usuario

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express'
+import { AuthenticateClientController } from './modules/account/authenticateClient/AuthenticateClientController';
 import { CreateClientController } from './modules/clients/useCases/createClient/CreateClientController';
 
 const routes = Router();
 
 const createClientController = new CreateClientController();
+const authenticateClientUseCase = new AuthenticateClientController()
+
+routes.post("/authenticate", authenticateClientUseCase.handle)
+
 routes.post("/client/", createClientController.handle)
+
 
 export {routes}
