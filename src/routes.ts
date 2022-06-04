e18621fa45c98361d15809183b206d3ae93b2c4c
Ericson Scodeler <ericson.br@hotmail.com>
2022-06-04 17:37:56 -0300

    rota para verificar todas entregas atrelada a um deliveryman

--- src/routes.ts
+++ src/routes.ts
@@ -4,29 +4,41 @@ import { ensureAuthenticateDeliveryman } from './middlewares/ensureAuthenticateD
 import { AuthenticateClientController } from './modules/account/authenticateClient/AuthenticateClientController';
 import { AuthenticateDeliverymanController } from './modules/account/authenticateDeliveryman/AuthenticateDeliverymanController';
 import { CreateClientController } from './modules/clients/useCases/createClient/CreateClientController';
+import { FindAllDeliveriesController } from './modules/clients/useCases/deliveries/FIndAllDeliveriesController';
 import { CreateDeliveryController } from './modules/deliveries/useCases/createDelivery/CreateDeliveryController';
 import { FindAllAvailableController } from './modules/deliveries/useCases/findAllAvailable/FindAllAvailableController';
+import { UpdateDeliverymanController } from './modules/deliveries/useCases/updateDeliveryman/UpdateDeliverymanController';
 import { CreateDeliverymanController } from './modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController';
+import { FindAllDeliveriesDeliverymanController } from './modules/deliveryman/useCases/FindAllDeliveries/FindAllDeliveriesDeliverymanController';
 
 const routes = Router();
 
 const createClientController = new CreateClientController();
 const authenticateClientController = new AuthenticateClientController();
+const findAllDeliveriesClient = new FindAllDeliveriesController();
 
 const createDeliverymanController = new CreateDeliverymanController();
 const authenticateDeliverymanController = new AuthenticateDeliverymanController();
 
 const deliveryController = new CreateDeliveryController();
 const findAllAvailableController = new FindAllAvailableController();
+const updateDeliverymanController = new UpdateDeliverymanController();
 
-routes.post("/client/authenticate", authenticateClientController.handle)
-routes.post("/deliveryman/authenticate", authenticateDeliverymanController.handle)
+const findAllDeliveriesDeliveryman = new FindAllDeliveriesDeliverymanController()
 
 routes.post("/client", createClientController.handle)
+routes.post("/client/authenticate", authenticateClientController.handle)
+routes.get("/client/deliveries", ensureAuthenticateClient, findAllDeliveriesClient.handle)
+
 routes.post("/deliveryman", createDeliverymanController.handle)
+routes.post("/deliveryman/authenticate", authenticateDeliverymanController.handle)
+routes.get("/deliveryman/deliveries", ensureAuthenticateDeliveryman,findAllDeliveriesDeliveryman.handle)
 
 routes.post("/delivery", ensureAuthenticateClient, deliveryController.handle)
 routes.get("/delivery/available",ensureAuthenticateDeliveryman, findAllAvailableController.handle)
+routes.put("/delivery/updateDeliveryman/:id",ensureAuthenticateDeliveryman, updateDeliverymanController.handle)
+
+
 
 
 export { routes }
